f4cc1d7e066e3e19c2603765ff971e326743f9be
Hayes McCardell <hmccardell@catalystdevworks.com>
2016-07-01 10:59:40 -0700

    configured pom for log4j2 support, added log4j2 config file.  configured pom for postgres support, added applications properties to configure the database

--- src/main/java/com/catalyst/overwatch/schedule/model/Schedule.java
+++ src/main/java/com/catalyst/overwatch/schedule/model/Schedule.java
@@ -6,7 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by bpyl on 6/14/2016.
@@ -19,6 +21,11 @@ public class Schedule implements Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+
+    @NotNull(message = "Start date cannot be null")
+    private Date startDate;
+
+    private Date endDate;
 
     public long getId() {
         return id;
