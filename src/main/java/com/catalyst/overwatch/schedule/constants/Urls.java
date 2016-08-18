21f917748eeac6eef55aa3c96e40a760915323bc
jordan_barroga <jordan_barroga@choicehotels.com>
2016-08-18 11:04:31 -0700

    Parameterizing the urls.

--- src/main/java/com/catalyst/overwatch/schedule/constants/Urls.java
+++ src/main/java/com/catalyst/overwatch/schedule/constants/Urls.java
@@ -17,18 +17,60 @@ public class Urls {
 
     private static final String FRONT_END_BASE_URL = "http://%s:%s";
 
+    private static final String SURVEY_ENDPOINT = "http://%s:%s/surveys";
+
+    public static final String RESPONSE_BASE_URL = "http://%s:%s";
+
+    public static final String SURVEY_RESPONSE = "/surveyResponses";
+
+    public static final String QUERY_BY_DATE = "/search/findSurveyResponseByDateAnswered?dateAnswered=";
+
+    @Value("${frontend.domain}")
+    private String frontendDomain;
+
+    @Value("${frontend.port}")
+    private String frontendPort;
+
     @Value("${notification.domain}")
     private String notificationDomain;
 
     @Value("${notification.port}")
     private String notificationPort;
 
+    @Value("${survey.domain}")
+    private String surveyDomain;
+
+    @Value("${survey.port}")
+    private String surveyPort;
+
+    @Value("${response.domain}")
+    private String responseDomain;
+
+    @Value("${response.port}")
+    private String responsePort;
+
     public String getNotificationEndpoint() {
-        return String.format(NOTIFICATION_ENDPOINT,notificationDomain,notificationPort);
+        return String.format(NOTIFICATION_ENDPOINT, notificationDomain, notificationPort);
     }
 
     public String getFrontEndBaseUrl() {
-        return String.format(FRONT_END_BASE_URL,notificationDomain,notificationPort);
+        System.out.println("\t\tFRONT_END_BASE_URL: " + String.format(FRONT_END_BASE_URL,notificationDomain,notificationPort));
+        return String.format(FRONT_END_BASE_URL, frontendDomain, frontendPort);
+    }
+
+    public String getSurveyEndpoint() {
+        System.out.println("\t\tSURVEY_ENDPOINT: " + String.format(SURVEY_ENDPOINT, surveyDomain, surveyPort));
+        return String.format(SURVEY_ENDPOINT, surveyDomain, surveyPort);
+    }
+
+    public String getResponseBaseUrl() {
+        System.out.println("\t\tRESPONSE_BASE_URL: " + String.format(RESPONSE_BASE_URL, responseDomain, responsePort));
+        return String.format(RESPONSE_BASE_URL, responseDomain, responsePort);
+    }
+
+    public String getSearchSurveyResponseByDate() {
+        System.out.println("\t\tSURVEY_RESPONSE_BY_DATE: " + getResponseBaseUrl() + SURVEY_RESPONSE + QUERY_BY_DATE);
+        return getResponseBaseUrl() + SURVEY_RESPONSE + QUERY_BY_DATE;
     }
 
 }
