f928449564b05dad89f1b0aa89cdbbfe1747423f
Andrew Rajigah <arajigah@catalystitservices.com>
2016-08-26 14:55:18 -0700

    fixed customnotificationparser constants

--- src/main/java/com/catalyst/overwatch/schedule/utilities/CustomNotificationParser.java
+++ src/main/java/com/catalyst/overwatch/schedule/utilities/CustomNotificationParser.java
@@ -39,15 +39,19 @@ public class CustomNotificationParser {
                 break;
 
             case "SURVEY_SUBJECT_SPD_TEAM":
-                surveySubjectValue = NotificationConstants.SURVEY_SUBJECT_SPRINT_PLANNING;
+                surveySubjectValue = NotificationConstants.SURVEY_SUBJECT_SPD_TEAM;
                 break;
 
             case "SURVEY_SUBJECT_SPD_LEADERS":
-                surveySubjectValue = NotificationConstants.SURVEY_SUBJECT_SPRINT_PLANNING_LEADS;
+                surveySubjectValue = NotificationConstants.SURVEY_SUBJECT_SPD_LEADERS;
                 break;
 
             case "SURVEY_SUBJECT_EM_QUANTITATIVE":
-                surveySubjectValue = NotificationConstants.SURVEY_SUBJECT_QUANTITATIVE;
+                surveySubjectValue = NotificationConstants.SURVEY_SUBJECT_EM_TL_QUANTITATIVE;
+                break;
+
+            case "SURVEY_SUBJECT_TL_QUANTITATIVE":
+                surveySubjectValue = NotificationConstants.SURVEY_SUBJECT_EM_TL_QUANTITATIVE;
                 break;
 
             default:
@@ -85,15 +89,19 @@ public class CustomNotificationParser {
                 break;
 
             case "SURVEY_BODY_SPD_TEAM":
-                surveyBodyValue = NotificationConstants.SURVEY_BODY_SPRINT_PLANNING;
+                surveyBodyValue = NotificationConstants.SURVEY_BODY_SPD_TEAM;
                 break;
 
             case "SURVEY_BODY_SPD_LEADERS":
-                surveyBodyValue = NotificationConstants.SURVEY_BODY_SPRINT_PLANNING_LEADS;
+                surveyBodyValue = NotificationConstants.SURVEY_BODY_SPD_LEADERS;
                 break;
 
             case "SURVEY_BODY_EM_QUANTITATIVE":
-                surveyBodyValue = NotificationConstants.SURVEY_BODY_QUANTITATIVE;
+                surveyBodyValue = NotificationConstants.SURVEY_BODY_EM_TL_QUANTITATIVE;
+                break;
+
+            case "SURVEY_BODY_TL_QUANTITATIVE":
+                surveyBodyValue = NotificationConstants.SURVEY_BODY_EM_TL_QUANTITATIVE;
                 break;
 
             default:
