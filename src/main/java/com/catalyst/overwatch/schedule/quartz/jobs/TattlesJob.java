0fabf36ae549fc4bde6dcb5f011a035bc0fc2363
jordan_barroga <jordan_barroga@choicehotels.com>
2016-09-12 11:15:37 -0700

    Changed the logging for when a call to the report service takes place.

--- src/main/java/com/catalyst/overwatch/schedule/quartz/jobs/TattlesJob.java
+++ src/main/java/com/catalyst/overwatch/schedule/quartz/jobs/TattlesJob.java
@@ -125,10 +125,11 @@ public class TattlesJob extends SchedulerBaseJob implements Job {
       flight.setIsClosed(true);
       flightRepository.save(flight);
 
+      logger.info("Retrieve a scheduling using id: " + id);
       Schedule scheduleById = scheduleRepository.findById(id);
 
       logger.info("Report endpoint url: " + urls.getReportEndpoint());
-      logger.info("Schedule retrieved using id "+ id + ": " + scheduleById.toString());
+      logger.info("Schedule retrieved: " + scheduleById.toString());
 
       logger.info(restTemplate.getForObject(urls.getReportEndpoint() + scheduleById.getTemplateUri(), Object.class).toString());
     }
