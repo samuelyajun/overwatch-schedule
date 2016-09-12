a019d2f4b25d71523f462e83eac96b9e4484b868
jordan_barroga <jordan_barroga@choicehotels.com>
2016-09-12 11:16:28 -0700

    Fixed a wording in a logger call.

--- src/main/java/com/catalyst/overwatch/schedule/quartz/jobs/TattlesJob.java
+++ src/main/java/com/catalyst/overwatch/schedule/quartz/jobs/TattlesJob.java
@@ -125,7 +125,7 @@ public class TattlesJob extends SchedulerBaseJob implements Job {
       flight.setIsClosed(true);
       flightRepository.save(flight);
 
-      logger.info("Retrieve a scheduling using id: " + id);
+      logger.info("Retrieve a schedule using id: " + id);
       Schedule scheduleById = scheduleRepository.findById(id);
 
       logger.info("Report endpoint url: " + urls.getReportEndpoint());
